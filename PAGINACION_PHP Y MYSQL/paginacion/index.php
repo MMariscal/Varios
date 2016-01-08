3bff990eb86164ecbb122130d1b376aac8a7d275
Marcos Mariscal <marcos.mariscal@icloud.com>
2016-01-08 15:41:20 +0100

    Modificar búsqueda

--- PAGINACION_PHP Y MYSQL/paginacion/index.php
+++ PAGINACION_PHP Y MYSQL/paginacion/index.php
@@ -5,7 +5,11 @@
             require_once("PHPPaging.lib/PHPPaging.lib.php");
             require_once("conexion/conexion.php");
             $pagina = new PHPPaging;
-            $pagina->agregarConsulta("select * from seguidores");
+            $pagina->agregarConsulta("$consulta = mysqli_query($connection, "SELECT * FROM PIEZA WHERE MODELO COLLATE UTF8_SPANISH_CI LIKE                              '$consultaBusqueda' OR MEDIDAS COLLATE UTF8_SPANISH_CI LIKE '$consultaBusqueda' OR USO COLLATE UTF8_SPANISH_CI LIKE '$consultaBusqueda'
+			OR SERIE COLLATE UTF8_SPANISH_CI LIKE '$consultaBusqueda' OR COLOR COLLATE UTF8_SPANISH_CI LIKE '$consultaBusqueda'
+			OR APLICACION COLLATE UTF8_SPANISH_CI LIKE '$consultaBusqueda' OR ESTILO COLLATE UTF8_SPANISH_CI LIKE '$consultaBusqueda'
+			OR IMAGEN COLLATE UTF8_SPANISH_CI LIKE '$consultaBusqueda' OR OTROS COLLATE UTF8_SPANISH_CI LIKE '$consultaBusqueda' OR CONCAT(modelo,'', medidas,'', uso,'', serie,'', color,'',
+			   aplicacion,'', estilo,'', imagen,'', otros) COLLATE UTF8_SPANISH_CI LIKE '%consultaBusqueda'");");
             $pagina->ejecutar();
             while($res=$pagina->fetchResultado()){
                 echo $res['nom_seguidor'].'<br>';
