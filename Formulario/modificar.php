f20b6390de35d0c81c8d7387ca2e42640c1b2786
Marcos Mariscal <marcos.mariscal@icloud.com>
2016-01-20 17:34:34 +0100

    Modificar formulario modificar.php

--- Formulario/modificar.php
+++ Formulario/modificar.php
@@ -25,17 +25,41 @@
 			<table width="50%">
 				<tr>
 					<input type="hidden" name="id" value="<?php echo $id; ?>">
-					<td width="20"><b>Usuario</b></td>
-					<td width="30"><input type="text" name="usuario" size="25" value="<?php echo $row['usuario']; ?>" /></td>
+					<td width="20"><b>MODELO</b></td>
+					<td width="30"><input type="text" name="modelo" size="75" value="<?php echo $row['MODELO']; ?>" /></td>
 				</tr>
 				<tr>
-					<td><b>Password</b></td>
-					<td><input type="password" name="password" size="25" value="<?php echo $row['contrasenia']; ?>" /></td>
+					<td width="20"><b>MEDIDAS</b></td>
+					<td width="30"><input type="text" name="medidas" size="75" value="<?php echo $row['MEDIDAS']; ?>" /></td>
 				</tr>
 				<tr>
-					<td><b>Email</b></td>
-					<td><input type="text" name="email" size="25" value="<?php echo $row['email']; ?>" /></td>
+					<td width="20"><b>USO</b></td>
+					<td width="30"><input type="text" name="uso" size="75" value="<?php echo $row['USO']; ?>" /></td>
 				</tr>
+				<tr>
+					<td width="20"><b>SERIE</b></td>
+					<td width="30"><input type="text" name="serie" size="75" value="<?php echo $row['SERIE']; ?>" /></td>
+				</tr>
+				<tr>
+					<td width="20"><b>COLOR</b></td>
+					<td width="30"><input type="text" name="color" size="75" value="<?php echo $row['COLOR']; ?>" /></td>
+				</tr>
+				<tr>
+					<td width="20"><b>APLICACION</b></td>
+					<td width="30"><input type="text" name="aplicacion" size="75" value="<?php echo $row['APLICACION']; ?>" /></td>
+				</tr>
+				<tr>
+					<td width="20"><b>ESTILO</b></td>
+					<td width="30"><input type="text" name="estilo" size="75" value="<?php echo $row['ESTILO']; ?>" /></td>
+				</tr>
+				<tr>
+					<td width="20"><b>IMAGEN</b></td>
+					<td width="30"><input type="text" name="imagen" size="75" value="<?php echo $row['IMAGEN']; ?>" /></td>
+				</tr>
+				<tr>
+					<td width="20"><b>OTROS</b></td>
+					<td width="30"><input type="text" name="otros" size="75" value="<?php echo $row['OTROS']; ?>" /></td>
+                </tr>
 				<tr>
 					<td colspan="2"><center><input type="submit" name="Guardar" value="Guardar" /></center></td>
 				</tr>
