e00155ed9fef89b74d269d5dc8ceae2c96ffbf90
Marcos Mariscal <marcos.mariscal@icloud.com>
2016-01-22 14:01:39 +0100

    navbar.php

--- rl-php-master/php/navbar.php
+++ rl-php-master/php/navbar.php
@@ -8,7 +8,7 @@
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" href="./"><b>MYAPP</b></a>
+    <a class="navbar-brand" href="./"><b>OcreAPP</b></a>
   </div>
 
   <!-- Collect the nav links, forms, and other content for toggling -->
