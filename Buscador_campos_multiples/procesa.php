f674a707b2e481e285f73aa360207c0710310a00
Marcos Mariscal <marcos.mariscal@icloud.com>
2016-02-01 13:40:15 +0100

    Comentar líneas para cambiar fichero. Las referencias a los if creo que sobran

--- Buscador_campos_multiples/procesa.php
+++ Buscador_campos_multiples/procesa.php
@@ -2,17 +2,33 @@
     $consulta = '';
     $parametros = '';
     if ($_POST['columna1'] != '') $parametros .= "columna1=" . $_POST['columna1'] .'&';
+var_dump($parametros);
+echo "<br>";
     if ($_POST['columna2'] != '') $parametros .= "columna2=" . $_POST['columna2'] .'&';
+var_dump($parametros);
+echo "<br>";
     $porciones = explode('&', $parametros);
+var_dump($porciones);
+echo "<br>";
     $cantidad = count($porciones)-1;
+var_dump($cantidad);
+echo "<br>";
     if ($cantidad > 1){
         for($i = 0; $i < $cantidad; $i++){
             $consulta .= $porciones[$i] . 'AND';
+            var_dump($consulta);
+            echo "<br>";
         }
     }else{
         $consulta .= $porciones[0] . 'AND';
+        var_dump($consulta);
+        echo "<br>";
     }
     $consulta = substr($consulta, 0, strlen($consulta) -4);
+var_dump($consulta);
+echo "<br>";
     echo $consulta .'<br><br>';
     $query_consulta = sprintf("SELECT * FROM TABLA WHERE $consulta");
+var_dump($query_consulta);
+echo "<br>";
 ?>
