de81e39f29024e6d66540c0b91e2af2282287f06
Marcos Mariscal <marcos.mariscal@icloud.com>
2016-02-17 14:02:07 +0100

    archivo_adjunto.php formulario.html
    pruebas de envío de ficheros adjuntos

--- envio_formulario/archivo_adjunto.php
+++ envio_formulario/archivo_adjunto.php
@@ -1,7 +1,9 @@
 <?php
-    ini_set("SMTP", "smtp.gmail.com");
-    ini_set("smtp_port", "localhost");
-    ini_set('sendmail_from', 'cti.mmariscal@gmail.com');
+    /*
+        ini_set("SMTP", "smtp.gmail.com");
+        ini_set("smtp_port", "localhost");
+        ini_set('sendmail_from', 'cti.mmariscal@gmail.com');
+    */
 
     // Variables para los campos de texto
     $nombre = strip_tags($_POST["first_name"]);
@@ -20,7 +22,7 @@
     $correoDestino = "marcos@ocrestudi.es";
 
     // Asunto del Correo
-    $asunto = "Enviado por " . $nombre . " " . $apellido;
+    $asunto = "Enviado por " . $nombre . " " . $apellidos;
     
     // -> Mensaje en formato MULTIPART MIME
     $cabecera = "MIME-VERSION: 1.0\r\n";
@@ -34,6 +36,34 @@
     $cuerpo .= "charset=utf-8\r\n";
     $cuerpo .= "Content-Transfer-Encoding: 8bit\r\n";
     $cuerpo .= "\r\n"; // línea vacía
+    $cuerpo .= "Correo enviado por: " .$nombre. " " .$apellidos;
+    $cuerpo .= "con fecha: " .$fechaFormato. "\r\n";
+    $cuerpo .= "Email: " .$mail. "\r\n";
+    $cuerpo .= "\r\n"; // línea vacía
 
-    
-    
+    // -> Segunda parte del mensaje (archivo adjunto)
+        // -> Encabezado
+    $cuerpo .= "--=C=T=E=C=\r\n";
+    $cuerpo .= "Content-Type: application/octet-stream; ";
+    $cuerpo .= "name= " .$nameFile. "\r\n";
+    $cuerpo .= "Content-Transfer-Encoding: base64\r\n";
+    $cuerpo .= "Content-Disposition: attachment; ";
+    $cuerpo .= "filename=" .$nameFile. "\r\n";
+    $cuerpo .= "\r\n"; // línea vacía
+
+    $fp = fopen($tempFile, "rb");
+    $file = fread($fp, $sizeFile);
+    $file = chunk_split(base64_encode($file));
+
+    $cuerpo .= "$file\r\n";
+    $cuerpo .= "\r\n"; // línea vacía
+    // Delimitador del final del mensaje
+    $cuerpo .= "--=C=T=E=C=--\r\n";
+
+    // Enciar el correo
+    if(mail($correoDestino, $asunto, $cuerpo, $cabecera)){
+        echo "Correo enviado";   
+    }else{
+        echo "Error enviando el correo";
+    }
+?>
