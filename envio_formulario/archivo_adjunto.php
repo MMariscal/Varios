5b827d39762548181f42f0ba79323da88bfb58e5
Marcos Mariscal <marcos.mariscal@icloud.com>
2016-02-17 13:27:07 +0100

    archivo_adjunto.php
    Redacción programa para envío

--- envio_formulario/archivo_adjunto.php
+++ envio_formulario/archivo_adjunto.php
@@ -11,4 +11,29 @@
 
     // Variables para los datos del archivo adjunto
     $nameFile = $_FILES['archivo']['name'];
-    $sizeFila = $_FILES['archivo']['size'];
+    $sizeFile = $_FILES['archivo']['size'];
+    $typeFile = $_FILES['archivo']['type'];
+    $tempFile = $_FILES['archivo']['tmp_name'];
+    $fecha = time();
+    $fechaFormato = date("j/n/Y", $fecha);
+
+    $correoDestino = "marcos@ocrestudi.es";
+
+    // Asunto del Correo
+    $asunto = "Enviado por " . $nombre . " " . $apellido;
+    
+    // -> Mensaje en formato MULTIPART MIME
+    $cabecera = "MIME-VERSION: 1.0\r\n";
+    $cabecera .= "Content-type: multipart/mixed;";
+    $cabecera .= "boundary=\"=C=T=E=C=\"\r\n";
+    $cabecera .= "From: {$mail}";
+
+    // Primera parte del cuerpo del mensaje
+    $cuerpo = "--=C=T=E=C=\r\n";
+    $cuerpo .= "Content-type: text/plain";
+    $cuerpo .= "charset=utf-8\r\n";
+    $cuerpo .= "Content-Transfer-Encoding: 8bit\r\n";
+    $cuerpo .= "\r\n"; // línea vacía
+
+    
+    
